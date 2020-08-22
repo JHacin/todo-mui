0c6269ac352770fa1f88e9c2eb7b62bdcb54c75f
JHacin <jan.hacin@gmail.com>
2020-08-22 22:10:01 +0200

    Setup MUI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
 import React, { FC } from 'react';
+import { AppBar, CssBaseline, Toolbar, Typography } from '@material-ui/core';
 
-export const App: FC = () => <div></div>;
+export const App: FC = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5">
+            Todo App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'fontsource-roboto';
+import 'material-design-icons/iconfont/material-icons.css';
 import { App } from './App';
 
 ReactDOM.render(
